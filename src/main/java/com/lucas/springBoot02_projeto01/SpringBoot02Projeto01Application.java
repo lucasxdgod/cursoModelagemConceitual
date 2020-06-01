d144a023c9af73e794b0c0a4e7bb9e91bbfab007
lucasxdgod <lucas12233314@gmail.com>
2020-05-31 20:49:09 -0400

    Projeto criado

---
+++ src/main/java/com/lucas/springBoot02_projeto01/SpringBoot02Projeto01Application.java
@@ -0,0 +1,13 @@
+package com.lucas.springBoot02_projeto01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02Projeto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot02Projeto01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/springBoot02_projeto01/SpringBoot02Projeto01ApplicationTests.java
@@ -0,0 +1,9 @@
+package com.lucas.springBoot02_projeto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot02Projeto01ApplicationTests {
+
+}
