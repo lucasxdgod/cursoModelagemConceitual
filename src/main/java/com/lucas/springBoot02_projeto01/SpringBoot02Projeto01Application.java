d1cb5d014ce94b86f6dd6796d3d2081d360a433d
lucasxdgod <lucas12233314@gmail.com>
2020-06-03 13:53:22 -0400

    Criando operacao de instanciacao

--- src/main/java/com/lucas/springBoot02_projeto01/SpringBoot02Projeto01Application.java
+++ src/main/java/com/lucas/springBoot02_projeto01/SpringBoot02Projeto01Application.java
@@ -1,13 +1,34 @@
 package com.lucas.springBoot02_projeto01;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBoot02Projeto01Application {
+import com.lucas.springBoot02_projeto01.domain.Categoria;
+import com.lucas.springBoot02_projeto01.repositoreis.CategoriaRepository;
 
+@SpringBootApplication
+public class SpringBoot02Projeto01Application implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot02Projeto01Application.class, args);
 	}
+	
+	
+	@Override
+	public void run(String...strings) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
 
 }
